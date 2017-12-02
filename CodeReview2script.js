231324d7d83b5be6d2f9a0b8ab22e95acfb153cd
erik djamgarian <djamgarian0702@gmail.com>
2017-12-02 16:20:52 +0100

    finished

---
+++ CodeReview2script.js
@@ -0,0 +1,31 @@
+/*var countries=["netherlands, united kingdom"];
+var city=["amsterdam, london"];
+
+var amsterdam = [1500, 500, 1800, 700];
+var london = [2500, 1500, 1000, 900];
+console.log(amsterdam, london);
+*/
+
+var dam1 = 1500;
+var dam2 = 500;
+var dam3 = 1800;
+var dam4 = 700;
+var amsterdam = [dam1 + dam2 + dam3 + dam4];
+console.log(amsterdam);
+var averagea = [amsterdam/4];
+console.log(averagea);
+document.getElementById("nl").innerHTML = averagea();
+
+var lnd1 = 2500;
+var lnd2 = 1500;
+var lnd3 = 1000;
+var lnd4 = 900;
+var london = [lnd1 + lnd2 + lnd3 + lnd4];
+console.log(london);
+var averagel = [london/4];
+console.log(averagel);
+document.getElementById("ad").innerHTML = averagel();
+
+
+
+
